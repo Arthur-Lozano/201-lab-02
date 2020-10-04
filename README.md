cda6d4c7c303c7661a1fa3691f83727e43b02717
Arthur Lozano <chillout885@gmail.com>
2020-10-03 22:21:53 -0700

    updated read me to show who I collaborated with during this lab along with the css and html page to better design the page and finally the javascript to increase the user interaction

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 201-lab-02
+# Worked with Jenner Dulce, Terrence Simms and Dawit Ayana
+# Also had help from TA Nicholas Ryan
--- js/app.js
+++ js/app.js
@@ -1,37 +1,105 @@
 'use strict';
 
+
+var points = 0;
 var userName = prompt('What is your name?');
 console.log('Hi ' + userName);
-
 // Question 1
 var yourCity = prompt('Do you like living in Seattle?').toLocaleLowerCase();
 if (yourCity === 'yes' || yourCity === 'y') {
   //console.log('Yes! I love living in Seattle.');
   alert('Yes! I love living in Seattle.');
+  points++;
 }
 // Question 2
 var favHobby = prompt('Do you have time these days for your hobbies?').toLocaleLowerCase();
 if (favHobby === 'yes' || favHobby === 'y') {
   //console.log('Yes!  Plenty of time.');
   alert('Yes! Plenty of time.');
+  points++;
 }
 // Question 3
 var justRelax = prompt('Is relaxing necessary for you each day?').toLocaleLowerCase();
 if (justRelax === 'yes' || justRelax === 'y') {
   //console.log('Yes! If you do not have a chance to relax you might not be able to re-charge your battery');
   alert('Yes! If you do not have a chance to relax you might not be able to re-charge your battery');
+  points++;
 }
 // Question 4
 var getReady = prompt('Do you like road trips?').toLocaleLowerCase();
 if (getReady === 'yes' || getReady === 'y') {
   //console.log('Yes! A chance to road trip is a chance to see life from a different point of view other than your original viewpoint.');
   alert('Yes! A chance to road trip is a chance to see life from a different point of view other than your original viewpoint.');
+  points++;
 }
 // Question 5
 var tripLocation = prompt('Would you like to drive along the coast?').toLocaleLowerCase();
 if (tripLocation === 'yes' || tripLocation === 'y') {
   //console.log('Yes! The coast is gorgeous.');
   alert('Yes! The coast is gorgeous.');
+  points++;
+}
+//Question 6
+var i = 0;
+var yourAttempt = 4;
+
+while (i < 4) {
+  var guess = prompt('Guess a number from 1 to 4 please');
+
+  if (guess > 3) {
+    alert('Sorry, that number is too high');
+  }
+
+  else if (guess < 3) {
+    alert('Sorry, that number is too low');
+  }
+
+  else if (guess = 3) {
+    alert('Correct! You guessed the right number');
+    points++;
+    break;
+  }
+
+  i++;
+  if (i === yourAttempt) {
+    alert('Sorry you maxed out your attempt, the correct answer was 3');
+  }
 }
 
-alert('Thanks ' + userName + ' for visiting my site');
+// Question 7
+var yourAttempt = 6;
+var bestSoup = ['cheese','clam chowder','fish','gumbo','chicken noodle'];
+var makeGuess = prompt('Try to guess some of my favorite soups!').toLowerCase();
+var correctGuess = false;
+
+while (yourAttempt > 0 && !correctGuess) {
+  yourAttempt--;
+
+  //made the correct guess
+  for (i < correctGuess.length; i++;) {
+    if (correctGuess[i] === makeGuess){
+      correctGuess = true;
+      alert(`Exactly!  I love ${makeGuess}`); //Notice the back ticks make this work not single quotes 
+      points++;
+    }
+  }
+
+  // if wrong
+  if (yourAttempt > 0 && !correctGuess) {
+    makeGuess = prompt(`No, I do not listen to ${makeGuess}`);
+  }
+  // No more attempts
+  if (yourAttempt === 0 && !correctGuess) {
+    alert(`Sorry, the correct answers were: ${bestSoup}`);
+  }
+}
+
+
+
+alert(`Thanks for answering, your score was${points}`);
+
+
+
+
+
+
